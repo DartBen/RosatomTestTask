5756e0a596d0f52ba2a3c7e141f88d2731c6ea02
D.Ryabov <ryabchikk@mail.ru>
2025-10-20 00:55:22 +0300

    маппинги (статичные)
    Signed-off-by: D.Ryabov <ryabchikk@mail.ru>

---
+++ RosatomTestTask/RosatomTestTask.Infrastructure/Mappings/DetailMapping.cs
@@ -0,0 +1,27 @@
+﻿using RosatomTestTask.Domain;
+using RosatomTestTask.Infrastructure.Entities;
+
+namespace RosatomTestTask.Infrastructure.Mappings
+{
+    public static class DetailMapping
+    {
+        public static DetailEntity ToEntity(this Detail detail, int masterId)
+        {
+            return new DetailEntity
+            {
+                Name = detail.Name,
+                Amount = detail.Amount,
+                MasterId = masterId
+            };
+        }
+
+        public static Detail ToDomain(this DetailEntity entity)
+        {
+            return new Detail
+            {
+                Name = entity.Name,
+                Amount = entity.Amount
+            };
+        }
+    }
+}
---
+++ RosatomTestTask/RosatomTestTask.Infrastructure/Mappings/MasterMapping.cs
@@ -0,0 +1,30 @@
+﻿using RosatomTestTask.Domain;
+using RosatomTestTask.Infrastructure.Entities;
+
+namespace RosatomTestTask.Infrastructure.Mappings
+{
+    public static class MasterMapping
+    {
+        public static MasterEntity ToEntity(this Master master)
+        {
+            return new MasterEntity
+            {
+                Number = master.Number,
+                Date = master.Date,
+                Amount = master.Amount,
+                Note = master.Note
+            };
+        }
+
+        public static Master ToDomain(this MasterEntity entity)
+        {
+            return new Master
+            {
+                Number = entity.Number,
+                Date = entity.Date,
+                Amount = entity.Amount,
+                Note = entity.Note
+            };
+        }
+    }
+}
