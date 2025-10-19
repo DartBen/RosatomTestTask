a6275b512f9aca92ed6563295027dba0ea2328ae
D.Ryabov <ryabchikk@mail.ru>
2025-10-20 00:54:21 +0300

    Добвил Доменные сущности

---
+++ RosatomTestTask/RosatomTestTask.Domain/Detail.cs
@@ -0,0 +1,8 @@
+﻿namespace RosatomTestTask.Domain
+{
+    public class Detail
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+    }
+}
---
+++ RosatomTestTask/RosatomTestTask.Domain/Master.cs
@@ -0,0 +1,10 @@
+﻿namespace RosatomTestTask.Domain
+{
+    public class Master
+    {
+        public string Number { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string? Note { get; set; }
+    }
+}
