f8403626d62e0de6c0438e719768063ad71ef3e2
D.Ryabov <ryabchikk@mail.ru>
2025-10-22 16:32:56 +0300

    миграции

---
+++ RosatomTestTask/RosatomTestTask.Infrastructure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosatomTestTask.Infrastructure
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<RosatomTestTaskDbContext>
+    {
+        public RosatomTestTaskDbContext CreateDbContext(string[] args)
+        {
+            // Берём строку подключения из переменной окружения
+            var connectionString = Environment.GetEnvironmentVariable("DBConnectionString")
+                ?? throw new InvalidOperationException("DBConnectionString environment variable is not set.");
+
+            var dataSource = new NpgsqlDataSourceBuilder(connectionString)
+                .EnableDynamicJson()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<RosatomTestTaskDbContext>();
+            optionsBuilder.UseNpgsql(dataSource);
+
+            return new RosatomTestTaskDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ RosatomTestTask/RosatomTestTask.Infrastructure/Migrations/20251022130532_InitialCreate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RosatomTestTask.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Masters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Number = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    Note = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Masters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Details",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    MasterId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Details", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Details_Masters_MasterId",
+                        column: x => x.MasterId,
+                        principalTable: "Masters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Details_MasterId",
+                table: "Details",
+                column: "MasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Masters_Number",
+                table: "Masters",
+                column: "Number",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Details");
+
+            migrationBuilder.DropTable(
+                name: "Masters");
+        }
+    }
+}
